fbd556a0b89713d48023be48c26d929e3c2570a1
Harshit Rai <raiharshit66@gmail.com>
2025-06-06 15:00:38 +0530

    Minor fixes

--- Strawberry/frontend/src/App.tsx
+++ Strawberry/frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
-import Navbar from './components/shared/Navbar'
 import Main from './components/shared/Main'
 const App = () => {
   return (
     <div>
-      <Main/>
+      <Main />
     </div>
   )
 }
