9c5768b75763c36965e2f001ea9769d37d121c40
Harshit Rai <raiharshit66@gmail.com>
2025-06-06 13:30:37 +0530

    Render the main compponent on the APP tsx

--- Strawberry/frontend/src/App.tsx
+++ Strawberry/frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
 import Navbar from './components/shared/Navbar'
-
+import Main from './components/shared/Main'
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Main/>
     </div>
   )
 }
