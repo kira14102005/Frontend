fb42ea38ea87799e16ee4a6935bda0e064dda6e4
Harshit Rai <raiharshit66@gmail.com>
2025-06-06 14:49:35 +0530

    Import the mui icons and complete the SideBar Menu

--- Strawberry/frontend/src/components/shared/Main.tsx
+++ Strawberry/frontend/src/components/shared/Main.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import Navbar from './Navbar'
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const Main = () => {
     const [sidebar, setSidebar] = useState(false)
     let component;
-    if (sidebar) component = ;
+    if (sidebar) component = <SideBar />;
     else component = <div></div>
     return (
         <main className='bg-primaryDark md:py-6 md:px-12'>
             <section className='relative min-h-[650px] bg-gradient-to-r from-primary to-secondary w-full md:rounded-xl shadow-md flex flex-col justify-center'>
-                <Navbar />
+                <Navbar setSidebar={setSidebar} />
                 <CentralComponent />
                 <h1 className='text-center font-bold  text-white text-[80px] uppercase w-full  absolute bottom-0 sm:text-[100px] md:text-[120px] xl:[150px] z-0 text-shadow'>berries</h1>
                 {component}
@@ -26,7 +28,7 @@ export function CentralComponent() {
                     01____________
                 </h1>
                 <h1 className='text-shadow-lg/30 text-5xl flex flex-wrap uppercase font-bold text-shadow'>
-                    A HELTHY FRUIT
+                    A HEALTHY FRUIT
                 </h1>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem excepturi nihil eius temporibus perspiciatis aut, dicta velit cum nesciunt sapiente.
@@ -48,5 +50,21 @@ export function CentralComponent() {
     </>
 }
 
+export function SideBar() {
+    return <>
+        <div className='absolute top-0 right-0 h-full w-[10%] bg-gradient-to-b from-primary to-secondary z-10 md:rounded-xl'>
+            <div className='w-full  h-full flex flex-col justify-center items-center space-y-2'>
+                <div className='w-[2px] bg-white h-[20%]'></div>
+                <div className='flex space-y-3 flex-col items-center text-white'>
+                    <div className='bg-transparent border-white border-[1px] aspect-square rounded-full w-[35px] flex items-center justify-center'>  <GitHubIcon /></div>
+                    <div className='bg-transparent border-white border-[1px] aspect-square rounded-full w-[35px] flex items-center justify-center'>                     <InstagramIcon />
+                    </div>
+                    <div className='bg-transparent border-white border-[1px] aspect-square rounded-full w-[35px] flex items-center justify-center'>  <LinkedInIcon /></div>
+                </div>
+                <div className='w-[2px] bg-white h-[20%]'></div>
+
+            </div>
+        </div></>
+}
 
 export default Main
