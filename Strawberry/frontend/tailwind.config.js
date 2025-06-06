825a8f67209b24fadd9c70716b73a9d7d7fdc8fb
Harshit Rai <raiharshit66@gmail.com>
2025-06-06 13:05:50 +0530

    Add the default themes and classes deafault styules to the config js

--- Strawberry/frontend/tailwind.config.js
+++ Strawberry/frontend/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#d22715",
+        secondary: "#980a07",
+        primaryDark: "#7e1108"
+      },
+      container: {
+        center: true,
+        padding: {
+          default: "1rem",
+          sm: "2rem"
+        }
+      }
+    },
   },
   plugins: [],
 }
