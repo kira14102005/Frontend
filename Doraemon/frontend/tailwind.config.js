e18d9f042c2f084449ba5c56d3b4216370fce757
Harshit Rai <raiharshit66@gmail.com>
2025-06-06 18:44:20 +0530

    Add the theme color names and preferred styling to the tailwind config js

--- Doraemon/frontend/tailwind.config.js
+++ Doraemon/frontend/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1A7EBE",
+        secondary: "#6B7280",
+        offWhite: "#E8EBF0",
+        titleBlack: "#4B5563"
+      }
+      ,
+      container: {
+        padding: {
+          default: "1rem",
+          sm: "2rem"
+        }
+
+      }
+    },
   },
   plugins: [],
 }
