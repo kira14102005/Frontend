763d19e04c0913c2e21b0d186b958ff0b37b7f93
Harshit Rai <raiharshit66@gmail.com>
2025-06-06 19:31:58 +0530

    Complete the Navbar styling

--- Doraemon/frontend/src/components/shared/Navbar.tsx
+++ Doraemon/frontend/src/components/shared/Navbar.tsx
@@ -19,7 +19,17 @@ const Navbar = () => {
                 <input type="text" className=
                     'rounded-full hidden lg:block lg:col-span-2 px-6 py-2 text-[20px] text-[#7D828C] bg-[#E2E8F0]' placeholder="Search" />
                 <div className="mx-2 justify-center items-center col-span-1 text-sm text-white">
-                    <div className="w-fit p-2 bg-primary aspect-square rounded-full fill-white hover:cursor-pointer"><svg stroke-width="0" viewBox="0 0 512 512" height="1em" className="text-2xl"width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg></div>
+                    <div className="w-fit p-2 bg-primary aspect-square rounded-full fill-white hover:cursor-pointer">
+                        <svg stroke-width="0" viewBox="0 0 512 512" height="1em" className="text-2xl" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
+                    </div>
+                </div>
+                <div className="col-span-1 items-center justify-center flex flex-row">
+                    <span className="text-secondary uppercase text-[24px]">eng</span>
+                    <span className="hover:cursor-pointer">
+                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="text-2xl fill-primary" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="none" d="M0 0h24v24H0z"></path><path d="m7 10 5 5 5-5z"></path>
+                        </svg>
+                    </span>
                 </div>
             </div>
         </div>
