04ffa658a93b65ac52379a06cdf9255ba00cfa57
Harshit Rai <raiharshit66@gmail.com>
2025-06-07 17:58:11 +0530

    Add the default theme color settings to the tailwind config js

--- Ferrari/frontend/tailwind.config.js
+++ Ferrari/frontend/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F12524",
+        secondary: "#8B1314",
+        darkred: "#64191E",
+        lgred: "#FD6467",
+        lpink: "#FDD7DD",
+        yell: "#F9FA74",
+        offwhite: "#ECF0F3",
+        pink: "#E9BAC0"
+      },
+      container : {
+        padding : {
+          default : "1rem",
+          sm : "2rem"
+        }
+      }
+    },
   },
   plugins: [],
 }
