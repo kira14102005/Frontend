2596bc7c41a82c521ab0c0582ada711e6a3fd6af
Harshit Rai <raiharshit66@gmail.com>
2025-06-08 15:50:44 +0530

    Add the logo of the ferraari to the Navbar component

---
+++ Ferrari/frontend/src/components/shared/Navbar.tsx
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+  return (
+    <div className="">
+        <div className="flex flex-row gap-3 items-center p-3">
+        <div className="">
+            <img src="/logo.png" alt="" />
+        </div>
+        <ul className="bg:primary flex flex-row gap-3 items-center">
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CARS</li>
+        </ul>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
