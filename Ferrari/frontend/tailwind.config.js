9936119e40192f9a5cfd735c8442098e42eb1ea6
Harshit Rai <raiharshit66@gmail.com>
2025-06-08 16:19:18 +0530

    Correct the tailwind config

--- Ferrari/frontend/tailwind.config.js
+++ Ferrari/frontend/tailwind.config.js
@@ -9,17 +9,17 @@ export default {
       colors: {
         primary: "#F12524",
         secondary: "#8B1314",
-        darkred: "#64191E",
+        dark_red: "#64191E",
         lgred: "#FD6467",
         lpink: "#FDD7DD",
         yell: "#F9FA74",
         offwhite: "#ECF0F3",
         pink: "#E9BAC0"
       },
-      container : {
-        padding : {
-          default : "1rem",
-          sm : "2rem"
+      container: {
+        padding: {
+          default: "1rem",
+          sm: "2rem"
         }
       }
     },
