6b5997ce445872b80b6b56c391c7a401218b95c1
Harshit Rai <raiharshit66@gmail.com>
2025-06-10 02:12:45 +0530

    Use the Button in the Main section

--- Ferrari/frontend/src/components/shared/Main.tsx
+++ Ferrari/frontend/src/components/shared/Main.tsx
@@ -1,16 +1,17 @@
+import Button from './Button'
 import Navbar from './Navbar'
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const Main = () => {
     return (
         <div className="relative min-w-screen h-screen bg-dark_red">
             <main className='cover w-full md:absolute top-10 left-10  h-full md:w-[90%] h-[86%] md:rounded-2xl flex flex-col p-7 gap-20'>
                 <Navbar />
-                <section className='w-full justify-center flex md:justify-start md:pl-20'>
-                    <div className='text-6xl flex flex-col md:items-start items-center gap-5'>
+                <section className='w-full items-center flex flex-col gap-9 md:justify-start md:pl-20'>
+                    <div className='text-6xl flex flex-col md:items-start items-center gap-2'>
                         <span className='text-cut'>FERRARI</span>
                         <span className='text-outline  hover:text-white transition-colors duration-500 ease-in-out'>LA FERRARI</span>
                     </div>
-                    <div></div>
+                    <div><Button name='Explore' onclick={()=>{}}/></div>
 
                 </section>
             </main>
