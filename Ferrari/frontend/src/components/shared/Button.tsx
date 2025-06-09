ed0464f64efc4f47418913b0be571b543b9b7534
Harshit Rai <raiharshit66@gmail.com>
2025-06-10 02:36:24 +0530

    Add the forward arrow emoji to the button

--- Ferrari/frontend/src/components/shared/Button.tsx
+++ Ferrari/frontend/src/components/shared/Button.tsx
@@ -1,3 +1,4 @@
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 interface ButtonProps {
     onclick: () => void,
     name: string
@@ -5,7 +6,9 @@ interface ButtonProps {
 const Button = (props: ButtonProps) => {
     return (
         <div className="text-[24px]">
-            <button className='but px-4 py-2 rounded-full'>{props.name}</button>
+            <button className='but px-7 py-2 rounded-full flex items-center gap-3'>
+                <p>{props.name}</p>
+                <ArrowForwardIcon className=''/></button>
         </div>
     )
 }
