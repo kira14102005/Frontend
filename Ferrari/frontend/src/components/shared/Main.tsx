52428d6ed3244d9fe218a471700eb385bc76cb82
Harshit Rai <raiharshit66@gmail.com>
2025-06-10 02:36:43 +0530

    Correct the alignment of the Main section

--- Ferrari/frontend/src/components/shared/Main.tsx
+++ Ferrari/frontend/src/components/shared/Main.tsx
@@ -1,19 +1,27 @@
 import Button from './Button'
 import Navbar from './Navbar'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 const Main = () => {
     return (
-        <div className="relative min-w-screen h-screen bg-dark_red">
-            <main className='cover w-full md:absolute top-10 left-10  h-full md:w-[90%] h-[86%] md:rounded-2xl flex flex-col p-7 gap-20'>
+        <div className="md:px-4 md:py-8 w-full h-full bg-dark_red">
+            <main className='cover w-full h-full md:rounded-2xl flex flex-col p-7 gap-20'>
                 <Navbar />
-                <section className='w-full items-center flex flex-col gap-9 md:justify-start md:pl-20'>
+                <section className='w-full items-center flex flex-col gap-9 md:items-start md:pl-20'>
                     <div className='text-6xl flex flex-col md:items-start items-center gap-2'>
                         <span className='text-cut'>FERRARI</span>
                         <span className='text-outline  hover:text-white transition-colors duration-500 ease-in-out'>LA FERRARI</span>
                     </div>
-                    <div><Button name='Explore' onclick={()=>{}}/></div>
+                    <div><Button name='Explore' onclick={() => { }} /></div>
 
+                    <section className='w-full my-3 flex'>
+                        <div className='relative'>
+                            <div className='bg-black opacity-10 absolute z-0 w-full h-full'></div>
+                            <div className='p-8 z-10'> <h1 className='text-xl font-semibold'>FEATURES</h1>
+                                <p className='text-[15px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, amet. Officia, possimus fuga. Nihil animi impedit voluptates, enim error placeat voluptatem dolor. Praesentium officiis suscipit, itaque quisquam natus magnam unde. Quod laudantium laboriosam fugiat quisquam reiciendis velit itaque beatae expedita doloremque nostrum. </p></div>
+                        </div>
+                    </section>
                 </section>
+
             </main>
         </div>
     )
